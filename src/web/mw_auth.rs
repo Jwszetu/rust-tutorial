814ff114de56102cb069c05c7e1e25ad60015896
Jowi <jwszetu@gmail.com>
2023-11-13 22:38:51 +0000

    adding println

--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -9,10 +9,8 @@ pub async fn mw_require_auth<B>(
     req: Request<B>,
     next: Next<B>,
 ) -> Result<Response> {
+    println!("-->> {:<12} - mw_require_auth", "MIDDLEWARE");
     let auth_token = cookies.get(AUTH_TOKEN);
-
-
     auth_token.ok_or(Error::AuthFailNoAuthTokenCookie)?; 
-
     return Ok(next.run(req).await)
 }
